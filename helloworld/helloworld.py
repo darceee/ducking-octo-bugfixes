460cc0812a9e7e0227f7445d15bad2ddafc5677b
darceee <darcy@darcy-pc>
2013-09-01 23:08:51 -0300

    16

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,12 +1,16 @@
 import webapp2
+from google.appengine.api import urlfetch
 
+def experiment_interest_level():
+	data = urlfetch.fetch("http://www.reddit.com/r/MysteryManipulation/comments/1lj9pa/15/")
+	if data.status_code == 200:
+		return str(data.content).find('I think this would be easier to do using Ruby instead of Python.')
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
+        self.response.write(experiment_interest_level())
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
